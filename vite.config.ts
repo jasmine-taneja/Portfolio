b9baf5e4f88c08361fe8c5d32916f429a2746fab
Jasmine Taneja <jasmine.taneja07@gmail.com>
2025-08-30 00:00:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // 👈 since your repo is jasmine-taneja.github.io
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
